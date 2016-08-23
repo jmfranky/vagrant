3a42e26b2e31c050daa373aee9209cb8d748f359
Stefan Harris <stfnhh@gmail.com>
2016-08-23 13:40:47 -0400

    seperate apache and php scripts, remove hostname from all Vagrantfiles

--- scripts/node.sh
+++ scripts/node.sh
@@ -5,7 +5,7 @@ hash node 2>/dev/null || {
   curl --silent -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash > /dev/null 2>&1
   echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
   source /home/vagrant/.profile
-  nvm install 5.0 > /dev/null 2>&1
-  nvm use 5.0 > /dev/null 2>&1
-  nvm alias default 5.0 > /dev/null 2>&1
+  nvm install 4.5 > /dev/null 2>&1
+  nvm use 4.5 > /dev/null 2>&1
+  nvm alias default 4.5 > /dev/null 2>&1
 }
