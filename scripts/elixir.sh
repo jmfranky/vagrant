92e24baf7c96d5be9aa97fad549b6618c8963d7e
Stefan Harris <stfnhh@gmail.com>
2016-08-23 15:10:41 -0400

    updates to elixer box

--- scripts/elixir.sh
+++ scripts/elixir.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 hash elixir 2>/dev/null || {
-  cd /usr/bin
-  sudo mkdir elixir
-  cd /usr/bin/elixir
-  wget https://github.com/elixir-lang/elixir/releases/download/v1.3.2/Precompiled.zip
-  yum install -y unzip
-  unzip Precompiled.zip
-  echo 'export PATH="$PATH:/usr/bin/elixir/bin:/usr/bin/node-v6.1.0-linux-x64/bin"' > /etc/profile
-  source /etc/profile
+  cd /usr/bin > /dev/null 2>&1
+  sudo mkdir elixir > /dev/null 2>&1
+  cd /usr/bin/elixir > /dev/null 2>&1
+  wget https://github.com/elixir-lang/elixir/releases/download/v1.3.2/Precompiled.zip > /dev/null 2>&1
+  yum install -y unzip > /dev/null 2>&1
+  unzip Precompiled.zip > /dev/null 2>&1
+  echo 'export PATH="$PATH:/usr/bin/elixir/bin"' > /etc/profile
+  source /etc/profile > /dev/null 2>&1
 }
--- scripts/phoenix.sh
+++ scripts/phoenix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-hash mix 2>/dev/null || {
+if ! mix local | grep --quiet "phoenix.new"; then
   cd ~
   yes | mix local.hex
   yes | mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
   sudo yum install -y inotify-tools
-}
+fi
