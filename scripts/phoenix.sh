7e2e67359d3c8ee3800a1f2aa7d2c24963f79fcc
Stefan Harris <stfnhh@gmail.com>
2016-08-23 15:31:07 -0400

    updates to elixer box

--- scripts/phoenix.sh
+++ scripts/phoenix.sh
@@ -2,7 +2,7 @@
 
 if ! mix local | grep --quiet "phoenix.new"; then
   cd ~
-  yes | mix local.hex
-  yes | mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
-  sudo yum install -y inotify-tools
+  yes | mix local.hex > /dev/null 2>&1
+  yes | mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez > /dev/null 2>&1
+  sudo yum install -y inotify-tools > /dev/null 2>&1
 fi
