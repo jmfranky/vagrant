6657c6e140d3ef5a13d7a132755ce96f92e30888
Stefan Harris <stfnhh@gmail.com>
2016-08-24 10:44:15 -0400

    update php script

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# https://git.io/v65Jb
+
 hash httpd 2>/dev/null || {
   echo "Installing Apache"
   systemctl enable httpd.service > /dev/null 2>&1
   printf "NameVirtualHost *\n<VirtualHost *:80>\nDocumentRoot /var/www/html\n<Directory \"/var/www/html\">\nOptions FollowSymLinks\nAllowOverride ALL\n</Directory>\n</VirtualHost>" > /etc/httpd/conf.d/welcome.conf > /dev/null 2>&1
   sed -i "s/User apache/User vagrant/g" /etc/httpd/conf/httpd.conf > /dev/null 2>&1
   sed -i "s/Group apache/Group vagrant/g" /etc/httpd/conf/httpd.conf > /dev/null 2>&1
-  systemctl start httpd.service > /dev/null 2>&1
+  systemctl start httpd.service
 }
