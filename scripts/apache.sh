37a7e441f09a30c08a211fcd59f6ba3cb61cb4d0
Stefan Harris <stfnhh@gmail.com>
2016-08-24 11:13:45 -0400

    fix broken apache running check

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -2,11 +2,11 @@
 
 # https://git.io/v65Jb
 
-hash httpd 2>/dev/null || {
+if ! ps ax | grep -v grep | grep httpd > /dev/null; then
   echo "Installing Apache"
   systemctl enable httpd.service > /dev/null 2>&1
   printf "NameVirtualHost *\n<VirtualHost *:80>\nDocumentRoot /var/www/html\n<Directory \"/var/www/html\">\nOptions FollowSymLinks\nAllowOverride ALL\n</Directory>\n</VirtualHost>" > /etc/httpd/conf.d/welcome.conf > /dev/null 2>&1
   sed -i "s/User apache/User vagrant/g" /etc/httpd/conf/httpd.conf > /dev/null 2>&1
   sed -i "s/Group apache/Group vagrant/g" /etc/httpd/conf/httpd.conf > /dev/null 2>&1
   systemctl start httpd.service > /dev/null 2>&1
-}
+fi
