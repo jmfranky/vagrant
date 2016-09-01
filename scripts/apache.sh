f50860f5b2f9772a6dd35694a368dd706c00d4e5
Stefan Harris <stfnhh@gmail.com>
2016-09-01 13:14:32 -0400

    update apache syntax to be more readable

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -4,7 +4,16 @@
 
 if ! ps ax | grep -v grep | grep httpd > /dev/null; then
   systemctl enable httpd.service > /dev/null 2>&1
-  printf "NameVirtualHost *\n<VirtualHost *:80>\nDocumentRoot /var/www/html\n<Directory \"/var/www/html\">\nOptions FollowSymLinks\nAllowOverride ALL\n</Directory>\n</VirtualHost>" > /etc/httpd/conf.d/welcome.conf > /dev/null 2>&1
+  cat > /etc/httpd/conf.d/welcome.conf <<EOF
+NameVirtualHost *
+<VirtualHost *:80>
+  DocumentRoot /var/www/html
+  <Directory "/var/www/html">
+    Options FollowSymLinks
+    AllowOverride ALL
+  </Directory>
+</VirtualHost>
+EOF
   sed -i "s/User apache/User vagrant/g" /etc/httpd/conf/httpd.conf > /dev/null 2>&1
   sed -i "s/Group apache/Group vagrant/g" /etc/httpd/conf/httpd.conf > /dev/null 2>&1
   systemctl start httpd.service > /dev/null 2>&1
