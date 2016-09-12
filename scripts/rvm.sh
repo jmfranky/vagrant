3fddf4241e8090e4e688e3493b2a25b8d013d6ab
Stefan Harris <stfnhrrs@users.noreply.github.com>
2016-09-12 14:43:35 -0400

    Update rvm.sh

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -3,6 +3,6 @@
 # https://git.io/v65Uj
 
 hash rvm 2>/dev/null || {
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 > /dev/null 2>&1
+  gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 > /dev/null 2>&1
   curl -sSL https://get.rvm.io | bash -s stable --gems=bundler > /dev/null 2>&1
 }
