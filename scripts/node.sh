146b3bf96872303511f2f55a6b9cc77f4d056a32
Stefan Harris <stfnhh@gmail.com>
2016-09-19 16:06:14 -0400

    Have node use the latest LTS version automatically

--- scripts/node.sh
+++ scripts/node.sh
@@ -2,11 +2,13 @@
 
 # https://git.io/v65Ui
 
+
 hash node 2>/dev/null || {
-  curl --silent -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash > /dev/null 2>&1
+  curl --silent -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash > /dev/null 2>&1
   echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
   source /home/vagrant/.profile
-  nvm install 4.5 > /dev/null 2>&1
-  nvm use 4.5 > /dev/null 2>&1
-  nvm alias default 4.5 > /dev/null 2>&1
+  version=$(nvm ls-remote | grep "Latest LTS" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" | sed -e 's/[^0-9.]//g')
+  nvm install $version > /dev/null 2>&1
+  nvm use $version > /dev/null 2>&1
+  nvm alias default $version > /dev/null 2>&1
 }
