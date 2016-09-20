36d198e9020a3fb9876a35574fb0f091e0a0a99b
Stefan Harris <stfnhh@gmail.com>
2016-09-20 16:47:53 -0400

    install wget in provision script if missing

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -4,6 +4,11 @@ hash dialog 2>/dev/null || {
   brew install dialog > /dev/null 2>&1
 }
 
+hash wget 2>/dev/null || {
+  brew install wget > /dev/null 2>&1
+}
+
+
 scripts=("https://raw.githubusercontent.com/Mullen/vagrant/master/docker/Vagrantfile" "https://raw.githubusercontent.com/Mullen/vagrant/master/elixer-postgres/Vagrantfile" "https://raw.githubusercontent.com/Mullen/vagrant/master/lamp/Vagrantfile" "https://raw.githubusercontent.com/Mullen/vagrant/master/meanjs/Vagrantfile" "https://raw.githubusercontent.com/Mullen/vagrant/master/node-mongodb/Vagrantfile" "https://raw.githubusercontent.com/Mullen/vagrant/master/python/Vagrantfile" "https://raw.githubusercontent.com/Mullen/vagrant/master/ruby-mongodb/Vagrantfile" "https://raw.githubusercontent.com/Mullen/vagrant/master/ruby-mariadb/Vagrantfile" "https://raw.githubusercontent.com/Mullen/vagrant/master/ruby-postgres/Vagrantfile" "https://raw.githubusercontent.com/Mullen/vagrant/master/ruby-redis/Vagrantfile" "https://raw.githubusercontent.com/Mullen/vagrant/master/yeoman/Vagrantfile")
 
 dialog --clear --title " Vagrant Provisioner " --menu "Choose one of the following builds:" 20 60 10 \
