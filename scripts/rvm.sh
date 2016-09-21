582fe0a2b438bf56d4bb26943fd2e08848ad8450
Stefan Harris <stfnhh@gmail.com>
2016-09-21 16:10:42 -0400

    make no docs default for rvm install

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -5,4 +5,5 @@
 hash rvm 2>/dev/null || {
   curl -sSL https://rvm.io/mpapis.asc | gpg2 --import - > /dev/null 2>&1
   curl -sSL https://get.rvm.io | bash -s stable --gems=bundler > /dev/null 2>&1
+  echo "gem: --no-rdoc --no-ri" >> ~/.gemrc
 }
