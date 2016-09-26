c6e97bd56b82d4974b0763934ad5796c00baa84e
Stefan Harris <stfnhh@gmail.com>
2016-09-26 09:51:38 -0400

    update php to 5.6

--- scripts/php.sh
+++ scripts/php.sh
@@ -3,10 +3,10 @@
 # https://git.io/v65U9
 
 hash php 2>/dev/null || {
-  rpm -Uvh https://mirror.webtatic.com/yum/el7/epel-release.rpm > /dev/null 2>&1
-  rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm > /dev/null 2>&1
-  yum install php55w php55w-opcache -y --skip-broken > /dev/null 2>&1
+  rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm  > /dev/null 2>&1
+  rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm  > /dev/null 2>&1
+  yum install php56w php56w-opcache -y --skip-broken > /dev/null 2>&1
   yum install yum-plugin-replace -y > /dev/null 2>&1
-  yum replace php-common --replace-with=php55w-common -y > /dev/null 2>&1
-  yum install php55u-gd php55w-mysql php55w-mbstring -y > /dev/null 2>&1
+  yum replace php-common --replace-with=php56w-common -y > /dev/null 2>&1
+  yum install php56u-gd php56w-mysql php56w-mbstring php56w-intl -y > /dev/null 2>&1
 }
