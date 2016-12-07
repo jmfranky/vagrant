963311f678dfef1aaea590780e1808f733640acf
Stefan Harris <stfnhh@gmail.com>
2016-12-07 15:20:17 -0500

    update

--- scripts/virtualenv.sh
+++ scripts/virtualenv.sh
@@ -3,7 +3,5 @@
 # https://git.io/v1zOG
 
 hash virtualenv 2>/dev/null || {
-  virtualenv --python $(which python2.7) ~/.virtualenvs/app
-  wget -O /home/vagrant/venv https://git.io/v1zO0 
-  chmod +x /home/vagrant/venv
+  virtualenv --python $(which python2.7) /home/vagrant/.virtualenvs/app
 }
