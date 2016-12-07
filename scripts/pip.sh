afcba2f9e5ccd1d22c9be94b59e71a59bda842db
Stefan Harris <stfnhh@gmail.com>
2016-12-07 15:06:08 -0500

    update

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -7,4 +7,5 @@ hash pip 2>/dev/null || {
   rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm > /dev/null 2>&1
   yum install -y python-pip MySQL-python libpng-devel git mysql gcc-c++ mailcap automake mysql-devel python-devel libpng-devel libcurl-devel libxml2-devel openssl-devel libstdc++-devel > /dev/null 2>&1
   pip install --upgrade pip > /dev/null 2>&1
+  pip install virtualenv > /dev/null 2>&1
 }
--- scripts/virtualenv.sh
+++ scripts/virtualenv.sh
@@ -3,7 +3,6 @@
 # https://git.io/v1zOG
 
 hash virtualenv 2>/dev/null || {
-  sudo pip install virtualenv > /dev/null 2>&1
   virtualenv --python $(which python2.7) ~/.virtualenvs/app > /dev/null 2>&1
   wget -O /usr/local/bin/venv https://git.io/v1zO0 > /dev/null 2>&1
   chmod +x /usr/local/bin/venv > /dev/null 2>&1
