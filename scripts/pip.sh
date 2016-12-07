fa341d6fe16113a8a527d4546f55767e7da62cc0
Stefan Harris <stfnhh@gmail.com>
2016-12-07 13:12:36 -0500

    silence install of virtualenv

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -7,7 +7,7 @@ hash pip 2>/dev/null || {
   rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm > /dev/null 2>&1
   yum install -y python-pip MySQL-python libpng-devel git mysql gcc-c++ mailcap automake mysql-devel python-devel libpng-devel libcurl-devel libxml2-devel openssl-devel libstdc++-devel > /dev/null 2>&1
   pip install --upgrade pip > /dev/null 2>&1
-  pip install virtualenv
+  pip install virtualenv > /dev/null 2>&1
   if test -f "/home/vagrant/app/requirements.txt"; then
     pip install -r /home/vagrant/app/requirements.txt > /dev/null 2>&1
   fi
