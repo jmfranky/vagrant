45781a3aa2489a4e57ff8d12c9af3df89fb0d6e8
Stefan Harris <stfnhh@gmail.com>
2016-12-07 14:49:27 -0500

    update urls for virtualenv

--- scripts/virtualenv.sh
+++ scripts/virtualenv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# https://git.io/
+# https://git.io/v1zOG
 
 hash virtualenv 2>/dev/null || {
   pip install virtualenv > /dev/null 2>&1
   virtualenv --python $(which python2.7) ~/.virtualenvs/app > /dev/null 2>&1
-  wget -O /usr/local/bin/venv URL > /dev/null 2>&1
+  wget -O /usr/local/bin/venv https://git.io/v1zO0 > /dev/null 2>&1
   chmod +x /usr/local/bin/venv > /dev/null 2>&1
 }
