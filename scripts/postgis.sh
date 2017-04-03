b452577175e76f695141c688f1edf0546bca587c
andreasantarl <andrea.santarl@gmail.com>
2017-04-03 15:36:57 -0400

    Starter code for postgis

---
+++ scripts/postgis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://raw.githubusercontent.com/Mullen/vagrant/master/scripts/postgis.sh
+
+if ! yum list installed "postgis2_95" >/dev/null 2>&1; then
+  yum install postgis2_95 > /dev/null 2>&1
+fi
