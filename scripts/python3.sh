d99093101a30c5be42fdffd58f3915257069fbeb
Stefan Harris <stefan.harris@mullenlowe.com>
2017-05-19 13:31:50 -0400

    python updates

--- scripts/python3.sh
+++ scripts/python3.sh
@@ -1,10 +1,16 @@
 hash python3 2>/dev/null || {
-  sudo yum install -y epel-release > /dev/null 2>&1
-  sudo yum install -y python34 > /dev/null 2>&1
+  cd /usr/src
+  wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
+  tar xzf Python-3.6.1.tgz
+  cd Python-3.6.1
+  ./configure
+  make altinstall
+  # sudo yum install -y epel-release > /dev/null 2>&1
+  # sudo yum install -y python34 > /dev/null 2>&1
   alias python=$(which python3) > /dev/null 2>&1
 }
 
-hash pip 2>/dev/null || {
+hash pip3 2>/dev/null || {
   curl -o /tmp https://bootstrap.pypa.io/get-pip.py
   python3 /tmp/get-pip.py
   alias pip=$(which pip3) > /dev/null 2>&1
