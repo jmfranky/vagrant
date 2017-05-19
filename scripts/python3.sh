94d6bc887ed3bc08e121ac8285eebae60e2e2d74
Stefan Harris <stefan.harris@mullenlowe.com>
2017-05-19 15:48:15 -0400

    python3 updates

--- scripts/python3.sh
+++ scripts/python3.sh
@@ -1,10 +1,10 @@
-hash python3 2>/dev/null || {
+hash python3.6 2>/dev/null || {
   yum -y install https://centos7.iuscommunity.org/ius-release.rpm > /dev/null 2>&1
   yum -y install python36u  > /dev/null 2>&1
   alias python=$(which python3.6)
 }
 
-hash pip 2>/dev/null || {
+hash pip3.6 2>/dev/null || {
   yum install -y python36u-pip libpng-devel git gcc-c++ mailcap automake python36u-devel libpng-devel libcurl-devel libxml2-devel openssl-devel libstdc++-devel > /dev/null 2>&1
   alias pip=$(which pip3.6)
   pip install virtualenv > /dev/null 2>&1
