25d209bd2fe42fede464a4b770b3554517efcf00
Stefan Harris <stefan.harris@mullenlowe.com>
2017-05-19 14:40:42 -0400

    install gcc to compile python3

--- scripts/python3.sh
+++ scripts/python3.sh
@@ -8,13 +8,12 @@ hash python3 2>/dev/null || {
   make altinstall
   # sudo yum install -y epel-release > /dev/null 2>&1
   # sudo yum install -y python34 > /dev/null 2>&1
-  alias python=$(which python3) > /dev/null 2>&1
+  alias python=$(which python3.6) > /dev/null 2>&1
 }
 
 hash pip3 2>/dev/null || {
-  curl -o /tmp https://bootstrap.pypa.io/get-pip.py
-  python3 /tmp/get-pip.py
-  alias pip=$(which pip3) > /dev/null 2>&1
+  curl -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
+  /usr/local/bin/easy_install-3.6 pip
   pip install virtualenv > /dev/null 2>&1
-  yum install -y libpng-devel git mysql gcc-c++ mailcap automake python-devel libpng-devel libcurl-devel libxml2-devel openssl-devel libstdc++-devel > /dev/null 2>&1
+  yum install -y libpng-devel git mysql mailcap automake python-devel libpng-devel libcurl-devel libxml2-devel openssl-devel libstdc++-devel > /dev/null 2>&1
 }
