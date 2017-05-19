f112308b763872ad317760b4293754e60d4ee779
Stefan Harris <stefan.harris@mullenlowe.com>
2017-05-19 15:41:15 -0400

    install gcc to compile python3

--- scripts/python3.sh
+++ scripts/python3.sh
@@ -1,19 +1,11 @@
 hash python3 2>/dev/null || {
-  yum install -y gcc gcc-c++
-  cd /usr/src
-  wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
-  tar xzf Python-3.6.1.tgz
-  cd Python-3.6.1
-  ./configure
-  make altinstall
-  # sudo yum install -y epel-release > /dev/null 2>&1
-  # sudo yum install -y python34 > /dev/null 2>&1
-  alias python=$(which python3.6) > /dev/null 2>&1
+  yum -y install https://centos7.iuscommunity.org/ius-release.rpm > /dev/null 2>&1
+  yum -y install python36u  > /dev/null 2>&1
+  alias python=$(which python3.6)
 }
 
-hash pip3 2>/dev/null || {
-  curl -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
-  /usr/local/bin/easy_install-3.6 pip
+hash pip 2>/dev/null || {
+  yum install -y python36u-pip libpng-devel git gcc-c++ mailcap automake python36u-devel libpng-devel libcurl-devel libxml2-devel openssl-devel libstdc++-devel > /dev/null 2>&1
+  alias pip=$(which pip3.6)
   pip install virtualenv > /dev/null 2>&1
-  yum install -y libpng-devel git mysql mailcap automake python-devel libpng-devel libcurl-devel libxml2-devel openssl-devel libstdc++-devel > /dev/null 2>&1
 }
