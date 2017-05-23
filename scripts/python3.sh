0c70471dd307207172aeaad4f2be79533e46cae4
Stefan Harris <stefan.harris@mullenlowe.com>
2017-05-23 09:54:34 -0400

    make alais for python3 ^ pip for current env

--- scripts/python3.sh
+++ scripts/python3.sh
@@ -7,6 +7,7 @@
 hash python3.6 2>/dev/null || {
   yum -y install https://centos7.iuscommunity.org/ius-release.rpm > /dev/null 2>&1
   yum -y install python36u  > /dev/null 2>&1
+  alias python=$(which python3.6)
   echo "alias python=$(which python3.6)" > /etc/bashrc
   echo "alias python=$(which python3.6)" > /home/vagrant/.bashrc
   echo "alias python=$(which python3.6)" > /root/.bashrc
@@ -14,6 +15,7 @@ hash python3.6 2>/dev/null || {
 
 hash pip3.6 2>/dev/null || {
   yum install -y python36u-pip libpng-devel git gcc-c++ mailcap automake python36u-devel libpng-devel libcurl-devel libxml2-devel openssl-devel libstdc++-devel > /dev/null 2>&1
+  alias pip=$(which pip3.6)
   echo "alias pip=$(which pip3.6)" > /etc/bashrc
   echo "alias pip=$(which pip3.6)" > /home/vagrant/.bashrc
   echo "alias pip=$(which pip3.6)" > /root/.bashrc
