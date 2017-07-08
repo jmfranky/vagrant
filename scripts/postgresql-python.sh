6c7641d30c3ccff1f69120eb497d2e085e2a3eb5
Stefan Harris <stefan.harris@mullenlowe.com>
2017-07-08 07:57:59 -0400

    switch to python specific postgres install, causes to many issues with ruby

--- scripts/postgresql-python.sh
+++ scripts/postgresql-python.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# https://raw.githubusercontent.com/Mullen/vagrant/master/scripts/postgresql.sh
+# https://raw.githubusercontent.com/Mullen/vagrant/master/scripts/postgresql-python.sh
 
 hash psql 2>/dev/null || {
   rpm -ivh http://yum.postgresql.org/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-2.noarch.rpm > /dev/null 2>&1
-  yum install -y postgresql95 postgresql95-server postgresql95-libs postgresql95-contrib postgresql95-devel postgresql-devel > /dev/null 2>&1
+  yum install -y postgresql95 postgresql95-server postgresql95-libs postgresql95-contrib postgresql95-devel postgresql95-devel postgresql-devel > /dev/null 2>&1
   rm -rf /var/lib/pgsql/9.5/data > /dev/null 2>&1
   /usr/pgsql-9.5/bin/postgresql95-setup initdb > /dev/null 2>&1
   systemctl start postgresql-9.5 > /dev/null 2>&1
@@ -13,6 +13,8 @@ hash psql 2>/dev/null || {
   sudo -u postgres psql 2>/dev/null -c "alter user postgres with password 'vagrant'" > /dev/null 2>&1
   sudo -u postgres createuser -s vagrant > /dev/null 2>&1
   echo "host all all 0.0.0.0/0 trust" >> /var/lib/pgsql/9.5/data/pg_hba.conf
+  sed -i "s/peer/md5/g" /var/lib/pgsql/9.5/data/pg_hba.conf > /dev/null 2>&1
+  sed -i "s/ident/md5/g" /var/lib/pgsql/9.5/data/pg_hba.conf > /dev/null 2>&1
   echo "listen_addresses = '*'" >> /var/lib/pgsql/9.5/data/postgresql.conf
   systemctl restart postgresql-9.5 > /dev/null 2>&1
 }
