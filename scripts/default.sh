2ddfa9cd9f6ef6e54824c5504dcd6759264adb9f
Stefan Harris <stefan.harris@mullenlowe.com>
2017-07-26 09:05:21 -0400

    add vim and chrony to default install

--- scripts/default.sh
+++ scripts/default.sh
@@ -8,3 +8,13 @@ systemctl stop firewalld > /dev/null 2>&1
 hash git 2>/dev/null || {
   yum install -y git > /dev/null 2>&1
 }
+
+hash chronyd 2>/dev/null || {
+  yum install -y chrony
+  systemctl enable chronyd
+  systemctl start chronyd
+}
+
+hash vim 2>/dev/null || {
+  yum install -y vim
+}
