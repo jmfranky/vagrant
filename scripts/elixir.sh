cf70ee76ac38239e0bca8a327d84147e30a2d320
Stefan Harris <stefan.harris@mullenlowe.com>
2017-07-26 09:28:40 -0400

    update elixer to 1.5

--- scripts/elixir.sh
+++ scripts/elixir.sh
@@ -2,11 +2,11 @@
 
 # https://raw.githubusercontent.com/Mullen/vagrant/master/scripts/elixir.sh
 
-hash elixir 2>/dev/null || {
+hash iex 2>/dev/null || {
   cd /usr/bin > /dev/null 2>&1
-  sudo mkdir elixir > /dev/null 2>&1
+  mkdir elixir > /dev/null 2>&1
   cd /usr/bin/elixir > /dev/null 2>&1
-  wget https://github.com/elixir-lang/elixir/releases/download/v1.3.2/Precompiled.zip > /dev/null 2>&1
+  wget https://github.com/elixir-lang/elixir/releases/download/v1.5.0/Precompiled.zip > /dev/null 2>&1
   yum install -y unzip > /dev/null 2>&1
   unzip Precompiled.zip > /dev/null 2>&1
   echo 'export PATH="$PATH:/usr/bin/elixir/bin"' > /etc/profile
