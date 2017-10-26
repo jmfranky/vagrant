f7eac4eeed7b4cee092218f65e28ff35429cdcca
Stefan Harris <stefan.harris@mullenlowe.com>
2017-10-26 11:26:47 -0400

    fix pip

--- scripts/python3.sh
+++ scripts/python3.sh
@@ -13,6 +13,11 @@ hash python3.6 2>/dev/null || {
   echo "alias python=$(which python3.6)" >> /root/.bashrc
 }
 
-hash pip3 2>/dev/null || {
+hash pip3.6 2>/dev/null || {
   yum install -y python36u-pip libpng-devel git gcc-c++ mailcap automake python36u-devel libpng-devel libcurl-devel libxml2-devel openssl-devel libstdc++-devel > /dev/null 2>&1
+  alias pip=$(which pip3.6)
+  echo "alias pip=$(which pip3.6)" >> /etc/bashrc
+  echo "alias pip=$(which pip3.6)" >> /home/vagrant/.bashrc
+  echo "alias pip=$(which pip3.6)" >> /root/.bashrc
+  pip3.6 install virtualenv > /dev/null 2>&1
 }
