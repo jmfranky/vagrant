07d62b1ec590476fa686774e473d58c625ae2e9f
Stefan Harris <stefan.harris@mullenlowe.com>
2017-11-27 12:27:30 -0500

    update postgresql python to 10, spacing changes in elixir config

--- scripts/postgresql-python.sh
+++ scripts/postgresql-python.sh
@@ -3,18 +3,18 @@
 # https://raw.githubusercontent.com/Mullen/vagrant/master/scripts/postgresql-python.sh
 
 hash psql 2>/dev/null || {
-  rpm -ivh http://yum.postgresql.org/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-2.noarch.rpm > /dev/null 2>&1
-  yum install -y postgresql95 postgresql95-server postgresql95-libs postgresql95-contrib postgresql95-devel postgresql95-devel postgresql-devel > /dev/null 2>&1
-  rm -rf /var/lib/pgsql/9.5/data > /dev/null 2>&1
-  /usr/pgsql-9.5/bin/postgresql95-setup initdb > /dev/null 2>&1
-  systemctl start postgresql-9.5 > /dev/null 2>&1
-  systemctl enable postgresql-9.5 > /dev/null 2>&1
-  chkconfig postgresql-9.5 on > /dev/null 2>&1
-  sudo -u postgres psql 2>/dev/null -c "alter user postgres with password 'vagrant'" > /dev/null 2>&1
+  rpm -Uvh https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-1.noarch.rpm > /dev/null 2>&1
+  yum install -y postgresql10-server postgresql10 postgresql10-libs postgresql10-contrib postgresql10-devel > /dev/null 2>&1
+  rm -rf /var/lib/pgsql/10/data > /dev/null 2>&1
+  /usr/pgsql-10/bin/postgresql-10-setup initdb > /dev/null 2>&1
+  systemctl start postgresql-10 > /dev/null 2>&1
+  systemctl enable postgresql-10 > /dev/null 2>&1
+  chkconfig postgresql-10 on > /dev/null 2>&1
+  sudo -u postgres psql 2>/dev/null -c "alter user postgres with password 'postgres'" > /dev/null 2>&1
   sudo -u postgres createuser -s vagrant > /dev/null 2>&1
-  echo "host all all 0.0.0.0/0 trust" >> /var/lib/pgsql/9.5/data/pg_hba.conf
-  sed -i "s/peer/md5/g" /var/lib/pgsql/9.5/data/pg_hba.conf > /dev/null 2>&1
-  sed -i "s/ident/md5/g" /var/lib/pgsql/9.5/data/pg_hba.conf > /dev/null 2>&1
-  echo "listen_addresses = '*'" >> /var/lib/pgsql/9.5/data/postgresql.conf
-  systemctl restart postgresql-9.5 > /dev/null 2>&1
+  echo "host all all 0.0.0.0/0 trust" >> /var/lib/pgsql/10/data/pg_hba.conf
+  sed -i "s/peer/md5/g" /var/lib/pgsql/10/data/pg_hba.conf > /dev/null 2>&1
+  sed -i "s/ident/md5/g" /var/lib/pgsql/10/data/pg_hba.conf > /dev/null 2>&1
+  echo "listen_addresses = '*'" >> /var/lib/pgsql/10/data/postgresql.conf
+  systemctl restart postgresql-10 > /dev/null 2>&1
 }
