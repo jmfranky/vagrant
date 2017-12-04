685fc0a03f72a2561770c65898c96a1e2350254e
Stefan Harris <stefan.harris@mullenlowe.com>
2017-12-04 15:05:36 -0500

    sync time with host

--- scripts/default.sh
+++ scripts/default.sh
@@ -9,12 +9,6 @@ hash git 2>/dev/null || {
   yum install -y git > /dev/null 2>&1
 }
 
-hash chronyd 2>/dev/null || {
-  yum install -y chrony > /dev/null 2>&1
-  systemctl enable chronyd > /dev/null 2>&1
-  systemctl start chronyd > /dev/null 2>&1
-}
-
 hash vim 2>/dev/null || {
   yum install -y vim > /dev/null 2>&1
 }
