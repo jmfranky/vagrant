d7a819391ce5938b96759be8e70348e0c4915aef
Stefan Harris <stefan.harris@mullenlowe.com>
2018-01-25 11:38:25 -0500

    add crystal lang pgql setup

--- scripts/crystal.sh
+++ scripts/crystal.sh
@@ -1,4 +1,6 @@
 hash crystal 2>/dev/null || {
+  sudo yum groupinstall development tools
+  sudo yum install readline-devel sqlite-devel openssl-devel libyaml-devel gc-devel libevent-devel libyaml-devel openssl-devel sqlite-devel
   curl https://dist.crystal-lang.org/rpm/setup.sh | bash > /dev/null 2>&1
   yum install crystal -y > /dev/null 2>&1
 }
