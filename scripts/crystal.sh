da9fde5a17466d7b7eed7cb9d63e5905db2d04a4
Stefan Harris <stefan.harris@mullenlowe.com>
2018-01-26 10:35:06 -0500

    clean up crystal file

--- scripts/crystal.sh
+++ scripts/crystal.sh
@@ -1,6 +1,6 @@
 hash crystal 2>/dev/null || {
-  sudo yum groupinstall development tools -y > /dev/null 2>&1
-  sudo yum install readline-devel sqlite-devel openssl-devel libyaml-devel gc-devel libevent-devel libyaml-devel openssl-devel sqlite-devel -y > /dev/null 2>&1
-  curl https://dist.crystal-lang.org/rpm/setup.sh | bash > /dev/null 2>&1
-  yum install crystal -y > /dev/null 2>&1
-}
+  curl https://dist.crystal-lang.org/rpm/setup.sh | bash
+
+  yum groupinstall development tools -y
+  yum install readline-devel sqlite-devel openssl-devel libyaml-devel gc-devel libevent-devel libyaml-devel openssl-devel sqlite-devel crystal -y
+} &> /dev/null
