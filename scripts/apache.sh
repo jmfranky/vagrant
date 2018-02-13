49912f018091a01526aeeb03c9fad92304149192
Stefan <stfnhrrs@users.noreply.github.com>
2018-02-13 11:42:16 -0500

    Update apache.sh

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -2,10 +2,10 @@
 
 # https://raw.githubusercontent.com/Mullen/vagrant/master/scripts/apache.sh
 
+
 if ! ps ax | grep -v grep | grep httpd > /dev/null; then
-  {
-    systemctl enable httpd.service
-    cat > /etc/httpd/conf.d/welcome.conf <<EOF
+  systemctl enable httpd.service > /dev/null 2>&1
+  cat > /etc/httpd/conf.d/welcome.conf <<EOF
 NameVirtualHost *
 <VirtualHost *:80>
   DocumentRoot /var/www/html
@@ -15,10 +15,9 @@ NameVirtualHost *
   </Directory>
 </VirtualHost>
 EOF
-    sed -i "s/User apache/User vagrant/g" /etc/httpd/conf/httpd.conf
-    sed -i "s/Group apache/Group vagrant/g" /etc/httpd/conf/httpd.conf
-    sed -i "s/EnableSendfile on/EnableSendfile off/g" /etc/httpd/conf/httpd.conf
-    systemctl start httpd.service
-    ln -s /var/www/html /home/vagrant/app
-  } > /dev/null 2>&1
+  sed -i "s/User apache/User vagrant/g" /etc/httpd/conf/httpd.conf > /dev/null 2>&1
+  sed -i "s/Group apache/Group vagrant/g" /etc/httpd/conf/httpd.conf > /dev/null 2>&1
+  sed -i "s/EnableSendfile on/EnableSendfile off/g" /etc/httpd/conf/httpd.conf > /dev/null 2>&1
+  systemctl start httpd.service > /dev/null 2>&1
+  ln -s /var/www/html /home/vagrant/app > /dev/null 2>&1
 fi
