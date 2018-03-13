49faad93bdc9bc9750cb39afac78bdda8fdb806e
Stefan Harris <stefan.harris@mullenlowe.com>
2018-03-13 09:49:15 -0400

    add elasticsearch script

--- scripts/elasticsearch.sh
+++ scripts/elasticsearch.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# https://raw.githubusercontent.com/Mullen/vagrant/master/scripts/elasticsearch.sh
+
 curl -s "http://localhost:9200" 2>&1 > /dev/null
 if [ $? != 0 ]; then
   yum install java-1.8.0-openjdk.x86_64 -y
