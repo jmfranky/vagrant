402930a2b92f717fbf59854132464bc3e056d857
Stefan Harris <stefan.harris@mullenlowe.com>
2018-03-13 09:44:58 -0400

    add elasticsearch script

---
+++ scripts/elasticsearch.sh
@@ -0,0 +1,9 @@
+curl -s "http://localhost:9200" 2>&1 > /dev/null
+if [ $? != 0 ]; then
+  yum install java-1.8.0-openjdk.x86_64 -y
+  cd /tmp
+  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.1.1.rpm
+  rpm -ivh elasticsearch-5.1.1.rpm 
+  systemctl enable elasticsearch
+  systemctl start elasticsearch
+fi > /dev/null 2>&1
