bdd82f180db091b8bf13c209a14a880781b8728c
Stefan Harris <stefan.harris@mullenlowe.com>
2018-03-15 08:48:44 -0400

    set elasticsearch to version we use in production

--- scripts/elasticsearch.sh
+++ scripts/elasticsearch.sh
@@ -6,8 +6,9 @@ curl -s "http://localhost:9200" 2>&1 > /dev/null
 if [ $? != 0 ]; then
   yum install java-1.8.0-openjdk.x86_64 -y
   cd /tmp
-  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.1.1.rpm
-  rpm -ivh elasticsearch-5.1.1.rpm 
-  systemctl enable elasticsearch
-  systemctl start elasticsearch
+  wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.5.2.noarch.rpm
+  yum install elasticsearch-1.5.2.noarch.rpm -y
+  systemctl daemon-reload
+  systemctl enable elasticsearch.service
+  systemctl start elasticsearch.service
 fi > /dev/null 2>&1
