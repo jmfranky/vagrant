48f6f1d02dd6743459ee904558d5fa6e21bc1199
Stefan Harris <1240279+stfnhh@users.noreply.github.com>
2018-04-03 10:13:07 -0400

    roll back to pgsql 9.5

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -3,19 +3,16 @@
 # https://raw.githubusercontent.com/stfnhh/vagrant/master/scripts/postgresql.sh
 
 hash psql 2>/dev/null || {
-  rpm -Uvh https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-1.noarch.rpm
-  yum install -y postgresql10-server postgresql10 postgresql10-libs postgresql10-contrib postgresql10-devel
-  rm -rf /var/lib/pgsql/10/data
-  /usr/pgsql-10/bin/postgresql-10-setup initdb
-  systemctl start postgresql-10
-  systemctl enable postgresql-10
-  chkconfig postgresql-10 on
-  sudo -u postgres psql 2>/dev/null -c "alter user postgres with password 'postgres'"
-  sudo -u postgres createuser -s vagrant
-  echo "host all all 0.0.0.0/0 trust" >> /var/lib/pgsql/10/data/pg_hba.conf
-  sed -i "s/peer/md5/g" /var/lib/pgsql/10/data/pg_hba.conf
-  sed -i "s/ident/md5/g" /var/lib/pgsql/10/data/pg_hba.conf
-  echo "listen_addresses = '*'" >> /var/lib/pgsql/10/data/postgresql.conf
-  ln -s /usr/pgsql-10/bin/p* /usr/local/bin
-  systemctl restart postgresql-10
-} > /dev/null 2>&1
+  rpm -ivh http://yum.postgresql.org/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-2.noarch.rpm > /dev/null 2>&1
+  yum install -y postgresql95 postgresql95-server postgresql95-libs postgresql95-contrib postgresql95-devel postgresql-devel > /dev/null 2>&1
+  rm -rf /var/lib/pgsql/9.5/data > /dev/null 2>&1
+  /usr/pgsql-9.5/bin/postgresql95-setup initdb > /dev/null 2>&1
+  systemctl start postgresql-9.5 > /dev/null 2>&1
+  systemctl enable postgresql-9.5 > /dev/null 2>&1
+  chkconfig postgresql-9.5 on > /dev/null 2>&1
+  sudo -u postgres psql 2>/dev/null -c "alter user postgres with password 'vagrant'" > /dev/null 2>&1
+  sudo -u postgres createuser -s vagrant > /dev/null 2>&1
+  echo "host all all 0.0.0.0/0 trust" >> /var/lib/pgsql/9.5/data/pg_hba.conf
+  echo "listen_addresses = '*'" >> /var/lib/pgsql/9.5/data/postgresql.conf
+  systemctl restart postgresql-9.5 > /dev/null 2>&1
+}
