6ff99c2e31d72dc874135e725957bd3ed991b173
Stefan Harris <1240279+stfnhh@users.noreply.github.com>
2018-05-22 09:24:47 -0400

    fix issue with crystal failint to compile because of incompatible version of libgc-dev

--- scripts/crystal.sh
+++ scripts/crystal.sh
@@ -6,5 +6,7 @@ hash crystal 2>/dev/null || {
   curl https://dist.crystal-lang.org/rpm/setup.sh | bash
 
   yum groupinstall -y development tools
-  yum install -y readline-devel sqlite-devel openssl-devel libyaml-devel gc-devel libevent-devel libyaml-devel openssl-devel sqlite-devel crystal
+  yum install -y readline-devel sqlite-devel openssl-devel libyaml-devel gc-devel libevent-devel libyaml-devel openssl-devel sqlite-devel 
+  yum remove -y libgc
+  yum install -y crystal
 } > /dev/null 2>&1
